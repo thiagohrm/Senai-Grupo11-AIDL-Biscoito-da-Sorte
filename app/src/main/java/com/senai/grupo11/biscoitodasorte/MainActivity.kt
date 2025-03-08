86088ed91fd8cc17fa6751ab98efe525b2b88f29
nockerajf <130074714+nockerajf@users.noreply.github.com>
2025-03-08 16:28:23 -0300

    Animation (#5)
    * Animation
    
    * Animation bug
    
    ---------
    
    Co-authored-by: Adriano Silva <asilv562@ford.com>

--- app/src/main/java/com/senai/grupo11/biscoitodasorte/MainActivity.kt
+++ app/src/main/java/com/senai/grupo11/biscoitodasorte/MainActivity.kt
@@ -8,13 +8,17 @@ import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
 import com.senai.grupo11.biscoitodasorte.service.BiscoitoServiceWrapper
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     private val TAG = "BiscoitoDaSorte.MainActivity"
     private val service = BiscoitoServiceWrapper()
+    private var isTextVisible = false
+
+    private val ANIMATION_DURATION = 500L
+    private val RESET_DURATION = 200L
+    private val TRANSLATION_X = -570f
 
     override fun onStart() {
         Log.i(TAG, "onStart()")
@@ -28,11 +32,14 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // Inflate o layout XML
-        service.state.observe(this){
+        setContentView(R.layout.activity_main)
+
+        val fortuneMessageTextView: TextView = findViewById(R.id.fortuneMessageTextView)
+        val revealFortuneButton: Button = findViewById(R.id.revealFortuneButton)
+
+        service.state.observe(this) {
             Log.i(TAG, "state: $it")
             if (it) {
                 lifecycleScope.launch {
@@ -40,18 +47,51 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        val fortuneMessageTextView: TextView = findViewById(R.id.fortuneMessageTextView)
-        val revealFortuneButton: Button = findViewById(R.id.revealFortuneButton)
 
         revealFortuneButton.setOnClickListener {
-            fortuneMessageTextView.text = ""
-            fortuneMessageTextView.visibility = View.VISIBLE // Torna o TextView visível
-            service.requestText()
+            if (isTextVisible) {
+                hideAndResetTextView(fortuneMessageTextView)
+            } else {
+                showTextViewWithAnimation(fortuneMessageTextView)
+            }
         }
-        service.textoDaSorte.observe(this){
+
+        service.textoDaSorte.observe(this) {
             Log.i(TAG, "textoDaSorte: $it")
-            revealFortuneButton.text = it
+            fortuneMessageTextView.text = it
         }
     }
-}
 
+    private fun hideAndResetTextView(view: TextView) {
+        view.animate()
+            .translationX(0f)
+            .setDuration(ANIMATION_DURATION)
+            .withEndAction {
+                view.visibility = View.INVISIBLE
+                isTextVisible = false
+                resetTextViewAndAnimateIn(view)
+            }
+    }
+
+    private fun resetTextViewAndAnimateIn(view: TextView) {
+        service.requestText()
+        view.visibility = View.INVISIBLE
+        view.animate()
+            .translationX(0f)
+            .setDuration(RESET_DURATION)
+            .withEndAction {
+                showTextViewWithAnimation(view)
+            }
+    }
+
+    private fun showTextViewWithAnimation(view: TextView) {
+        service.requestText()
+        view.visibility = View.VISIBLE
+        view.animate()
+            .translationX(TRANSLATION_X)
+            .setDuration(ANIMATION_DURATION)
+            .withEndAction {
+                isTextVisible = true
+            }
+    }
+}
